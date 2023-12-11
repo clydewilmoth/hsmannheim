f7084ffd30ae48b1be5a95e74c18e079f49ef9c9
selim <selimyag.eser@gmail.com>
2023-12-11 17:35:42 +0100

    DatenklassenÜbung

---
+++ src/DatenklassenÜbung.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DatenklassenÜbung {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        GameCharacter Selim = new GameCharacter(99, 5, 40, 65, 9);
+        GameCharacter Ruven = new GameCharacter(80, 15, 45, 75, 30);
+        GameCharacter Paul = new GameCharacter(90, 40, 70, 25, 20);
+
+        confidenceTest(Ruven);
+
+    }
+
+    public static void confidenceTest(GameCharacter character){
+
+        if(character.ppSize>20)
+            System.out.println("Way tooooo confident! DAYUMM BOII! \n8===============D ----");
+        else if(character.ppSize<=20&&character.ppSize>18)
+            System.out.println("Prinzipiell zufrieden, aber man könnte sich mehr vorstellen o.O");
+        else
+            System.out.println("Unglaublich unzufrieden, vielleicht sollte man eine Penisverlängerung in Köln in Betracht ziehen :/");
+
+    }
+
+}
---
+++ src/GameCharacter.java
@@ -0,0 +1,15 @@
+public class GameCharacter {
+    int attack;
+    int defense;
+    int health;
+    int speed;
+    int ppSize;
+    public GameCharacter(int attack, int defense, int health, int speed, int ppSize){
+        this.attack = attack;
+        this.defense = defense;
+        this.health = health;
+        this.speed = speed;
+        this.ppSize = ppSize;
+    }//constructor
+
+}
